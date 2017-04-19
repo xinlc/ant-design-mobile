c42e8fb7ef9ad99b5b6937af9357b254dfd5d509
silentcloud <rjmuqiang@gmail.com>
2017-04-19 16:18:40 +0800

    support web accessibility

--- components/slider/index.web.tsx
+++ components/slider/index.web.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import RcSlider from 'rc-slider/lib/Slider';
+import RcSlider from 'rc-slider';
 import SliderProps from './PropsType';
+
+const Handle = RcSlider.Handle;
 
 export default class Slider extends React.Component<SliderProps, any> {
   static defaultProps = {
     prefixCls: 'am-slider',
   };
   render() {
+    const { min, max } = this.props;
+    const handle = (props) => {
+      const { disabled, value, ...restProps } = props;
+      delete restProps.dragging;
+      return (
+        <Handle
+          role="slider"
+          aria-valuenow={value}
+          aria-disabled={disabled}
+          aria-valuemax={max}
+          aria-valuemin={min}
+          {...restProps}
+        />
+      );
+    };
     return (
-      <div className={`${this.props.prefixCls}-wrapper`}><RcSlider {...this.props} /></div>
+      <div className={`${this.props.prefixCls}-wrapper`}>
+        <RcSlider {...this.props} handle={handle} />
+      </div>
     );
   }
 }
